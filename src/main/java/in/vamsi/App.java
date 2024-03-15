1920d47bb103d8ca76ebcc7627f384eaf40a0e54
Vivenbrzbb <vivenbobby@gmail.com>
2024-03-15 16:28:02 +0530

    first commit

---
+++ src/main/java/in/vamsi/App.java
@@ -0,0 +1,13 @@
+package in.vamsi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
